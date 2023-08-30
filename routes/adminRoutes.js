c6d6b6b5f9d9be610181966bfb342229f89f0239
mangaljeet <mangaljeet.pande@databell.in>
2023-08-29 19:06:42 -0700

    admin

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get('/admin', (req, res)=>
 {
-    res.status(2000).send({
+    res.status(200).send({
         message: 'hii from admin side',
         code: 200
     })
