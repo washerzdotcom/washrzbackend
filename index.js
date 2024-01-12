a0624061df2c107fb0610d7d4a9304a8f205e19e
Ayush Singh <ayush.singh@databell.in>
2024-01-12 17:20:04 +0530

    changes in index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import customerRoutes from "./routes/customerRoutes.js";
 import AppError from "./utills/appError.js";
 import http from "http";
 import cors from "cors";
+import os from "os";
 import { Server } from "socket.io";
 const app = express();
 import cookies from "cookie-parser";
@@ -13,15 +14,15 @@ const server = http.createServer(app);
 app.use(cookies());
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://washrz.vercel.app'],
+  origin: ['https://washrz.vercel.app', 'http://localhost:3000'],
   methods: 'GET, POST, PUT, DELETE',
   credentials: true, // Allow credentials (cookies) to be sent with the request
 }));
 
-
+console.log(`The total number of CPUs is ${os.cpus().length}`);
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:3000', 'https://washrz.vercel.app'],
+    origin: ['http://localhost:3000', 'https://washrz.vercel.app', 'http://deploy-washrz-frontend.s3-website.ap-south-1.amazonaws.com'],
     credentials: true,
   },
   allowEIO3: true,
@@ -32,12 +33,22 @@ const addSocketToRequest = (io) => {
     next();
   };
 };
+app.get("/heavy", (req, res) => {
+  let total = 18;
+  for (let i = 0; i < 5000000000; i++) {
+    total++;
+  }
+  res.send(`The result of the CPU intensive task is ${total}\n`);
+});
+
 app.get("/test", (req, res) => {
   res.send({
     message: "api is workng",
     code: 200,
   });
 });
+
+
 app.use(express.json());
 app.use(addSocketToRequest(io));
 app.use("/api/v1", customerRoutes);
